65b52165efb64bcccea179ffa5de7da168a9a070
hutchic <chutchic@gmail.com>
2017-09-06 19:57:55 +0000

    Add the ability to run the docker-compose commands

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from compose.cli.command import get_project
 from flask import Flask
 from flask_hookserver import Hooks
 from git import Repo
@@ -19,8 +20,13 @@ def ping(data, guid):
 @hooks.hook('pull_request')
 def pull_request(data, guid):
     if data['action'] == 'opened' or data['action'] == 'reopened':
+      import pdb
+      pdb.set_trace()
       repo = Repo.clone_from(data['repository']['ssh_url'], '/tmp/' + str(data['pull_request']['id']), None, env={'GIT_SSH_COMMAND': 'ssh -i /home/ubuntu/.ssh/id_rsa' })
       repo.remotes.origin.fetch('+refs/pull/*:refs/heads/pull/*')
       git = repo.git
       git.checkout('pull/' + str(data['number']) + '/merge')
+      project = get_project('/tmp/' + data['pull_request']['id'] + '/')
+      project.build()
+      project.up()
       
